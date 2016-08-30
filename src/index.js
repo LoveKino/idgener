4d24b3ba66950a8c5df9058bf1e43a57b259044f
loveKino <842914439@qq.com>
2016-08-30 11:04:07 +0800

    time

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@
 
 let count = 0;
 
-module.exports = () => {
+module.exports = ({
+    timeVisual = false
+} = {}) => {
     count++;
     if (count > 10e6) {
         count = 0;
     }
     let rand = Math.random(Math.random()) + '';
 
-    return new Date().getTime() + '-' + count + '-' + rand;
+    let time = timeVisual ? getTimeStr() : new Date().getTime();
+
+    return `${time}-${count}-${rand}`;
+};
+
+let getTimeStr = () => {
+    let date = new Date();
+    return `${date.getFullYear()}_${date.getMonth()+1}_${date.getDate()}_${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}_${date.getMilliseconds()}`;
 };
